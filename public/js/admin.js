cd460612d990d4c81f6b6ecf97d923d6abb2bd01
Marius Larsen-Asp <marius.larsen.asp@outlook.com>
2015-10-25 15:53:05 +0100

    Update admin.js

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,18 +1,24 @@
 $(document).ready(function() {
+    // Method to get images on the admin page (for Approval of images)
     $.get('admin/data', function(data) {
         if(data.hasOwnProperty('error'))
         {
+            // Error occured, redirect to Home
             console.log(data.error);
             window.location.href = "home";
         }
         else
         {
+            // Loop each image that is waiting for admin approval
             $.each(data, function(i, item) {
+                // Apennd to #divAdminsPhotos
                 $('#divAdminPhotos').append('<div class="row photo-' + item.id + '"><div class="col-md-4"><img height="400" width="400" src="' + item.path + '" /></div></div><div class="row photo-' + item.id + '"><div class="col-md-5"><p><strong>Uploaded by <a data-token="{{ csrf_token() }}" href="mailto:'+item.email+'" target="_top">'+item.user_name+'</a></strong></p><a href="#" onclick="event.preventDefault();photoApprove(' + item.id + ')" class="btn btn-primary">Approve photo</a><a href="#" onclick="event.preventDefault();photoDelete(' + item.id + ')" class="btn btn-danger">Delete photo</a></div></div>');
             });
         }
     });
 });
+
+// Method to approve photo by ID (Ajax call) - This will update images table and make the photo show on the front page
 function photoApprove(id) {
     console.log("Approve "+ id);
     $.ajax({
@@ -26,6 +32,7 @@ function photoApprove(id) {
     });   
 }
 
+// Method to delete photo (ajax call)
 function photoDelete(id) {
     console.log("Delete "+ id);
     $.ajax({
