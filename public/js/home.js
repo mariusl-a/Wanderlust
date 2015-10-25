c36f38220cba2743d7894ca696e9fdbaec8bb9e0
Marius Larsen-Asp <marius.larsen.asp@outlook.com>
2015-10-25 15:56:18 +0100

    Update home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,9 @@
+// Method to load images on the front page
 $(document).ready(function() {
+    // ImageController index will give all images in array json object
     $.get('image', function(data) {
         $.each(data, function(i, item) {
+            // Append to the divHomePhotos div
             $('#divHomePhotos').append('<img class="col-md-4 col-xs-12" src="' + item.url + '" />');
         });
     });
